dd974f09a744fea424255b88495360a5950f4aa2
jimmyalvarez <jimmyalvarezjr@gmail.com>
2021-03-31 11:38:10 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const removeMain = document.getElementById("main");
+
+removeMain.remove();
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.innerHTML = '<h1>"Jimmy is the champion!"</h1>';
+
+newHeader.id = 'victory';
